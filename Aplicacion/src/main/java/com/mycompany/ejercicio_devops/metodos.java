90dfe302854cbaac12eb66cc73608b4560fea712
Pablo Acero <120599773+Paablus0@users.noreply.github.com>
2025-10-01 16:28:49 +0200

    introducir datos

--- Aplicacion/src/main/java/com/mycompany/ejercicio_devops/metodos.java
+++ Aplicacion/src/main/java/com/mycompany/ejercicio_devops/metodos.java
@@ -3,11 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.ejercicio_devops;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author daw2
  */
 public class metodos {
-    
+   
+    public void añadirDatos(Tarea t) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Tarea> listaPersonas = new ArrayList<>();
+        
+        System.out.print("Titulo: ");
+        String titulo = sc.nextLine();
+        
+        System.out.print("estado (true/false): ");
+        boolean estado = Boolean.parseBoolean(sc.nextLine());
+        
+        System.out.print("Descripcion: ");
+        String descripcion = sc.nextLine();
+       Tarea persona = new Tarea(titulo, descripcion);
+        persona.setEstado(estado); 
+        
+        listaPersonas.add(persona);
+    }
 }
+
+
+
+
+    
